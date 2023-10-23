c092b97c17faa2fba5c2e790cc34ca627432db35
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-10-23 19:35:40 +1000

    added DIP example

---
+++ seminar_03/DIP/diesel_engine.py
@@ -0,0 +1,6 @@
+from seminar_03.DIP.engine import Engine
+
+
+class DieselEngine(Engine):
+    def start(self):
+        print("Завели дизельный двигатель")
---
+++ seminar_03/DIP/petrol_engine.py
@@ -0,0 +1,6 @@
+from seminar_03.DIP.engine import Engine
+
+
+class PetrolEngine(Engine):
+    def start(self):
+        print("Завели бензиновый двигатель")
