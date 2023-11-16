fe4e005816f9a3d1b24ba098c295323418eff61f
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-11-17 02:16:05 +1000

    added seminar_10

---
+++ seminar_10/architecture_po_10.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `Connection` DROP FOREIGN KEY `Connection_fk0`;
+
+ALTER TABLE `Connection` DROP FOREIGN KEY `Connection_fk1`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk0`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk1`;
+
